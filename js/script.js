a2adbd7d351a01147601f6b3992b41343dac18d5
Rabia Yousuf <53699291+rabiayousuf187@users.noreply.github.com>
2023-05-21 10:31:59 +0500

    CHapter 47 to 48
    Chapter 47 Events: mouse 
     * onMouseover
     *onMouseout
    
    Chapter 48 Events: Feild
     * onFocus
     *onBlur
     main

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
 // Chapter 47 Events: mouse
 console.log("Chapter 47 Events: mouse");
+function show(){
+    // console.log("show");
+    // this.style.color = "red";
+    document.getElementById('img-show').style.display = "block";
+
+}
+
+function hide(){
+    console.log("Mouse out");
+    document.getElementById('img-show').style.display = "none";
+
+}
+
+function inpFeild(){
+    console.log("on Focus");
+    document.getElementById('email').style.border = "3px solid red";
+
+}
+
+function outFeild(){
+    console.log("on Blur");
+    document.getElementById('email').style.border = "1px solid black";
+
+}
